90a1ec2c513af6fa73c76f10a973fef20cd88fbd
Anderson6919 <159493613+Anderson6919@users.noreply.github.com>
2024-11-06 22:09:16 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Profile from "./components/Profile"
+
+export default function App(){
+  return(
+    <body>
+      <Profile img={'./ad.jpg'} name={'Anderson Barbosa'} bio={'Apaixonado por programção'} phone={'+ 55 (85) 94077280'} email={'anderson231@gmail.com'} githubUrl={'https://github.com/Anderson6919'} InstagramUrl={'b'}></Profile>
+    </body> 
+  )
+}
